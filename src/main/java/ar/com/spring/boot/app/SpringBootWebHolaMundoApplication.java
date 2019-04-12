5255648f440ff44960c971768b16406ffe1b924a
maximilianorodrigosoria <maximilianorodrigosoria@gmail.com>
2019-04-12 07:24:30 -0300

    Commit inicial

---
+++ src/main/java/ar/com/spring/boot/app/SpringBootWebHolaMundoApplication.java
@@ -0,0 +1,13 @@
+package ar.com.spring.boot.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebHolaMundoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebHolaMundoApplication.class, args);
+	}
+
+}
